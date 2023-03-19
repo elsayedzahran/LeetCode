07a2677f5fed348f37ebb85d44ac18f184329a5d
Elsayed Zahran <68614758+elsayedzahran@users.noreply.github.com>
2023-03-19 16:33:34 +0200

    Time: 2536 ms (14.08%), Space: 71.7 MB (91.75%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cs
@@ -0,0 +1,37 @@
+public class WordDictionary {
+    HashSet<string> dictionary;
+    public WordDictionary() {
+        dictionary = new HashSet<string>();
+    }
+    
+    public void AddWord(string word) {
+        dictionary.Add(word);
+    }
+    
+    public bool Search(string word) {
+        foreach (var item in dictionary){
+            if (item.Length == word.Length){
+                if (item == word) return true;
+                else{
+                    bool exist = true;
+                    for (int i = 0 ; i < item.Length ; i++){
+                        if (item[i] != word[i] && word[i] != '.') 
+                        {
+                            exist = false;
+                            break;
+                        }
+                    }
+                    if (exist) return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.AddWord(word);
+ * bool param_2 = obj.Search(word);
+ */
