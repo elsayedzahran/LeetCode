39b10a784418d087d302122662a38dc6f9a6fdf9
Elsayed Zahran <68614758+elsayedzahran@users.noreply.github.com>
2023-04-14 13:51:58 +0200

    Time: 82 ms (93.59%), Space: 55.7 MB (25.64%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int LongestPalindromeSubseq(string s) {
+        var n = s.Length;
+        var dp = new int[n][];
+        for (var i = 0; i < dp.Length; i++)
+            dp[i] = new int[n];
+
+        for (var i = n - 1; i >= 0; i--){
+            dp[i][i] = 1;
+            for (var j = i + 1; j < n; j++)
+            {
+                
+                if (s[i] == s[j])
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                else
+                    dp[i][j] = Math.Max(dp[i + 1][j], dp[i][j - 1]);
+             }
+        }
+
+        return dp[0][n - 1];
+    }
+}
