644c7c5be114998975f83e2de0d8805212edfb93
Elsayed Zahran <68614758+elsayedzahran@users.noreply.github.com>
2023-05-01 17:15:00 +0300

    Time: 80 ms (83.88%), Space: 38.2 MB (37.83%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cs
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cs
@@ -1,9 +1,5 @@
 public class Solution {
     public double Average(int[] salary) {
-        Array.Sort(salary);
-        int sum = 0;
-        for (int i = 1 ; i < salary.Length-1 ; i++)
-            sum += salary[i];
-        return (double)sum/(salary.Length - 2);
+        return (double)(salary.Sum() - salary.Max() - salary.Min())/(salary.Length-2);
     }
 }
