3f56650f85acb6b5b0a0451540e7cd77df06ed86
Elsayed Zahran <68614758+elsayedzahran@users.noreply.github.com>
2023-05-02 03:16:34 +0300

    Time: 87 ms (70.34%), Space: 40.1 MB (11.79%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cs
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cs
@@ -1,12 +1,8 @@
 public class Solution {
     public int ArraySign(int[] nums) {
-        int neg = 0;
-        foreach (int num in nums){
-            if (num < 0)
-                neg++;
-            if (num == 0)
-                return 0;
-        }
-        return neg % 2 == 0 ? 1 : -1;
+        if (nums.Contains(0)) 
+            return 0;
+        int count = nums.Count(p => p < 0);
+        return count % 2  == 0 ? 1 : -1;
     }
 }
